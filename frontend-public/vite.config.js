4fb23aba29926d529e7b49614285a87d4e82ba04
Gioele <gioele.stropeni@gmail.com>
2026-03-21 17:49:42 +0100

    fav icon, titolopagina e sitemap

--- frontend-public/vite.config.js
+++ frontend-public/vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://gstcodelab.com',
+      routes: [
+        '/',
+        '/servizi',
+        '/portfolio',
+        '/chi-sono',
+        '/contatti',
+        '/blog',
+        '/privacy-policy',
+      ],
+    }),
   ],
   server: {
     proxy: {
